11489654da544417af8e789a92f3c050a9c6b58a
Steve Bonds <sbonds@gmail.com>
2021-06-24 12:35:55 -0700

    PowerShell extension autoformat

--- azure-ad/expiring-app-credentials/Get-AzADAppExpiringCredentials.ps1
+++ azure-ad/expiring-app-credentials/Get-AzADAppExpiringCredentials.ps1
@@ -13,20 +13,22 @@ $appWithCredentials += $applications | Sort-Object -Property DisplayName | % {
     $application = $_
     $sp = $servicePrincipals | ? ApplicationId -eq $application.ApplicationId
     Write-Verbose ('Fetching information for application {0}' -f $application.DisplayName)
-    $application | Get-AzADAppCredential -ErrorAction SilentlyContinue | Select-Object -Property @{Name='DisplayName'; Expression={$application.DisplayName}}, @{Name='ObjectId'; Expression={$application.Id}}, @{Name='ApplicationId'; Expression={$application.ApplicationId}}, @{Name='KeyId'; Expression={$_.KeyId}}, @{Name='Type'; Expression={$_.Type}},@{Name='StartDate'; Expression={$_.StartDate -as [datetime]}},@{Name='EndDate'; Expression={$_.EndDate -as [datetime]}}
-  }
+    $application | Get-AzADAppCredential -ErrorAction SilentlyContinue | Select-Object -Property @{Name = 'DisplayName'; Expression = { $application.DisplayName } }, @{Name = 'ObjectId'; Expression = { $application.Id } }, @{Name = 'ApplicationId'; Expression = { $application.ApplicationId } }, @{Name = 'KeyId'; Expression = { $_.KeyId } }, @{Name = 'Type'; Expression = { $_.Type } }, @{Name = 'StartDate'; Expression = { $_.StartDate -as [datetime] } }, @{Name = 'EndDate'; Expression = { $_.EndDate -as [datetime] } }
+}
 
 Write-Host 'Validating expiration data...'
 $today = (Get-Date).ToUniversalTime()
 $limitDate = $today.AddDays($ExpiresInDays)
 $appWithCredentials | Sort-Object EndDate | % {
-        if($_.EndDate -lt $today) {
-            $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'Expired'
-        } elseif ($_.EndDate -le $limitDate) {
-            $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'ExpiringSoon'
-        } else {
-            $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'Valid'
-        }
+    if ($_.EndDate -lt $today) {
+        $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'Expired'
+    }
+    elseif ($_.EndDate -le $limitDate) {
+        $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'ExpiringSoon'
+    }
+    else {
+        $_ | Add-Member -MemberType NoteProperty -Name 'Status' -Value 'Valid'
+    }
 }
 
 $appWithCredentials
